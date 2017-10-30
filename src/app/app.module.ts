551a1da9e651a9f5f570ebc2e8354517b9e1f0fd
AnastasiyaBondar <bav7777777@gmail.com>
2017-10-30 17:47:27 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MdlModule } from '@angular-mdl/core';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {FooterComponent} from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
